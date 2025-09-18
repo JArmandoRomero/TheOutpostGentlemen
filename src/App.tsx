b152379bc0bd6dcfe378b4258801e202c17d2124
JArmandoRomero <JArmandoRomero@icloud.com>
2025-09-18 16:01:10 -0600

    initial comit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>The Outpost Gentlemen</h1>
+      <div className="card">Comming Soon!</div>
+    </>
+  );
+}
+
+export default App;
